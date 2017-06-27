8af1e8d8d3c507fbe5fa26ccc1f523a942131d1a
Tom Scott-Malden <tom@skywire.co.uk>
2017-06-27 08:58:18 +0100

    finished course

---
+++ src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Genus;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class GenusController extends Controller
+{
+
+	/**
+	 * @Route("/genus")
+	 */
+	public function listAction()
+	{
+		$em = $this->getDoctrine()->getManager();
+
+		$genuses = $em->getRepository('AppBundle:Genus')
+			->findAllPublishedOrderedBySize();
+
+		return $this->render('genus/list.html.twig', [
+			'genuses' => $genuses
+		]);
+	}
+
+	/**
+	 * @Route("/genus/new")
+	 */
+	public function newAction()
+	{
+		$genus = new Genus();
+		$genus->setName('Octopus'.rand(1, 100));
+		$genus->setSubFamily('Octopodinae');
+		$genus->setSpeciesCount(rand(100, 99999));
+
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($genus);
+		$em->flush();
+
+		return new Response('<html><body>Genus created!</body></html>');
+	}
+
+    /**
+     * @Route("/genus/{genusName}", name="genus_show")
+     */
+    public function showAction($genusName)
+    {
+    	$em = $this->getDoctrine()->getManager();
+    	$genus = $em->getRepository('AppBundle:Genus')
+		    ->findOneBy(['name' => $genusName]);
+
+	    if (!$genus) {
+		    throw $this->createNotFoundException('genus not found');
+	    }
+
+    	/*
+        $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
+        $key = md5($funFact);
+        if ($cache->contains($key)) {
+            $funFact = $cache->fetch($key);
+        } else {
+            sleep(1); // fake how slow this could be
+            $funFact = $this->get('markdown.parser')
+                ->transform($funFact);
+            $cache->save($key, $funFact);
+        }*/
+
+        $this->get('logger')
+            ->info('Showing genus: '.$genusName);
+
+        return $this->render('genus/show.html.twig', array(
+            'genus' => $genus
+        ));
+    }
+
+    /**
+     * @Route("/genus/{genusName}/notes", name="genus_show_notes")
+     * @Method("GET")
+     */
+    public function getNotesAction($genusName)
+    {
+        $notes = [
+            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
+            ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
+            ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
+        ];
+        $data = [
+            'notes' => $notes
+        ];
+
+        return new JsonResponse($data);
+    }
+}
---
+++ src/AppBundle/Entity/Genus.php
@@ -0,0 +1,132 @@
+<?php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * Class Genus
+ * @package AppBundle\Entity
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\GenusRepository")
+ * @ORM\Table(name="genus")
+ */
+Class Genus {
+
+	/**
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 * @ORM\Column(type="integer")
+	 */
+	private $id;
+
+	/**
+	 * @ORM\Column(type="string")
+	 */
+	private $name;
+
+	/**
+	 * @ORM\Column(type="string")
+	 */
+	private $subFamily;
+
+	/**
+	 * @ORM\Column(type="integer")
+	 */
+	private $speciesCount;
+
+	/**
+	 * @ORM\Column(type="string", nullable=true)
+	 */
+	private $funFact;
+
+	/**
+	 * @ORM\Column(type="boolean")
+	 */
+	private $isPublished;
+
+	/**
+	 * @return mixed
+	 */
+	public function getisPublished() {
+		return $this->isPublished;
+	}
+
+	/**
+	 * @param mixed $isPublished
+	 */
+	public function setIsPublished( $isPublished ) {
+		$this->isPublished = $isPublished;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getName() {
+		return $this->name;
+	}
+
+	/**
+	 * @param mixed $name
+	 */
+	public function setName( $name ) {
+		$this->name = $name;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getId() {
+		return $this->id;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getUpdatedAt() {
+		return new \DateTime('-'.rand(0, 100).' days');
+
+		//return $this->updatedAt;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getSubFamily() {
+		return $this->subFamily;
+	}
+
+	/**
+	 * @param mixed $subFamily
+	 */
+	public function setSubFamily( $subFamily ) {
+		$this->subFamily = $subFamily;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getSpeciesCount() {
+		return $this->speciesCount;
+	}
+
+	/**
+	 * @param mixed $speciesCount
+	 */
+	public function setSpeciesCount( $speciesCount ) {
+		$this->speciesCount = $speciesCount;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getFunFact() {
+		return $this->funFact;
+	}
+
+	/**
+	 * @param mixed $funFact
+	 */
+	public function setFunFact( $funFact ) {
+		$this->funFact = $funFact;
+	}
+}
